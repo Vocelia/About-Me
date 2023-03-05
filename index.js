a18a719ea8fb30fdf50b2cd4433524534baed60f
Vocelia <vocelia@outlook.jp>
2023-03-05 07:17:05 +0200

    pushing index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const path = require("path");
+const fs = require("fs");
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "templates"));
+app.use("/src", express.static(path.join(__dirname, "src")));
+
+app.get("/", (req, res) => {
+  let visitsFile = fs.readFileSync("./data/visits.json", "utf-8");
+	let visits = JSON.parse(visitsFile);
+  visits["visits"]++;
+  visitsFile = JSON.stringify(visits, null, 4);
+  fs.writeFileSync("./data/visits.json", visitsFile, "utf-8");
+  res.render("index");
+});
+
+app.get("/visits", (req, res) => {
+  let visitsFile = fs.readFileSync("./data/visits.json", "utf-8");
+	let visits = JSON.parse(visitsFile);
+  res.send(visits);
+});
+
+app.get("/chara", (req, res) => {
+  res.render("chara");
+});
+
+app.get("/email", (req, res) => {
+  res.send(`<center><h1><b>vocelia@outlook.jp</b></h1></center>`);
+});
+
+app.get("/matrix", (req, res) => {
+  res.send(`<center><h1><b>vocelia@matrix.org</b></h1></center>`);
+});
+
+app.get("/discord", (req, res) => {
+  res.send(`<center><h1><b>胡蝶#3100</b></h1></center>`);
+});
+
+http.listen(port, () => {
+  console.log("Listening on: ", port);
+});
